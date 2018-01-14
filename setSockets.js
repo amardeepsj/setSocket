f12e093fc2aa2111a80b8bc932133a805245131e
Amardeep Singh Jhajj <amardeep.singh.j@gmail.com>
2018-01-15 00:22:58 +0530

    commit setSockets.js

--- setSockets.js
+++ setSockets.js
@@ -1,12 +1,8 @@
-function setSockets(){
-    this.socket  = io("https://pricenotifications.herokuapp.com/");
-    setInterval(function() {jQuery('div.coin').children('b').each(function(){
-      var currency = jQuery(this).text().split(':')[0];
-      if(currency==="XRP/INR"){
-        ripple = {
-          price: jQuery(this).text().split(':')[1].trim()
-        }; 
-        socket.emit('sendPrices', ripple);
-      }
-    })}, 2000);
-}
+var script = document.createElement('script');
+script.innerHTML= "function setSocket() {this.socket  = io('https://pricenotifications.herokuapp.com/'); setInterval(function() {jQuery('div.coin').children('b').each(function(){var currency = jQuery(this).text().split(':')[0]; if(currency==='XRP/INR'){ ripple = {price: jQuery(this).text().split(':')[1].trim()}; socket.emit('sendPrices', ripple);} })}, 2000);} console.log('running')";
+document.body.appendChild(script);
+
+var script = document.createElement('script');
+script.setAttribute('src', 'https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.4/socket.io.js');
+script.setAttribute('onload', 'setSocket()');
+document.getElementsByTagName('head')[0].appendChild(script);
