584feb3280de8018ab27a589b65057f36f1093f4
Amardeep Singh Jhajj <amardeep.singh.j@gmail.com>
2018-01-22 02:33:32 +0530

    Updating for refreshing the page in time interval..

--- setSockets.js
+++ setSockets.js
@@ -1,8 +1,12 @@
 var script = document.createElement('script');
-script.innerHTML= "function setSocket() {this.socket  = io('https://pricenotifications.herokuapp.com/'); setInterval(function() {jQuery('div.coin').children('b').each(function(){var currency = jQuery(this).text().split(':')[0]; if(currency==='XRP/INR'){ ripple = {price: jQuery(this).text().split(':')[1].trim()}; socket.emit('sendPrices', ripple);} })}, 2000);} console.log('running')";
+script.innerHTML= "function setSocket() {this.socket  = io('https://pricenotifications.herokuapp.com/'); setInterval(function() {var sendInfo = {}; jQuery('div.coin').children('b').each(function(){var currency = jQuery(this).text().split(':')[0];if(currency==='XRP/INR'){ sendInfo.ripple = {price: jQuery(this).text().split(':')[1].trim(), currency: 'ripple'};}if(currency==='ETH/INR'){ sendInfo.ether = {price: jQuery(this).text().split(':')[1].trim(), currency: 'ether'};} });console.log('sendInfo ', sendInfo); socket.emit('sendPrices', sendInfo);}, 6000);} console.log('running')";
 document.body.appendChild(script);
 
 var script = document.createElement('script');
 script.setAttribute('src', 'https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.4/socket.io.js');
 script.setAttribute('onload', 'setSocket()');
 document.getElementsByTagName('head')[0].appendChild(script);
+
+setTimeout(function(){
+    location = ''
+}, 60000)
